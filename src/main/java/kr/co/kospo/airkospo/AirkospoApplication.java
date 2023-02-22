dac3063c4e0a8cebbf231f833add8dca5fe26753
soriwa <soriwa@gmail.com>
2023-02-22 23:44:24 +0900

    add: init

---
+++ src/main/java/kr/co/kospo/airkospo/AirkospoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kospo.airkospo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirkospoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirkospoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/kr/co/kospo/airkospo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package kr.co.kospo.airkospo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AirkospoApplication.class);
+	}
+
+}
